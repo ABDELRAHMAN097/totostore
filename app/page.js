05a17d9deef2994938c99059202ed710d86b154a
boody <bodymagdy97@gmail.com>
2024-08-21 16:45:59 +0300

    loading

--- app/page.js
+++ app/page.js
@@ -3,11 +3,15 @@
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebase";
+import { BarLoader } from "react-spinners";
 
 const page = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
 
   useEffect(() => {
+    setLoading(true);
     const fetchProducts = async () => {
       try {
         const querySnapshot = await getDocs(collection(db, "products"));
@@ -15,6 +19,7 @@ const page = () => {
           id: doc.id,
           ...doc.data()
         }));
+        setLoading(false);
         setProducts(productsData);
       } catch (error) {
         console.error("Error fetching products:", error);
@@ -26,19 +31,24 @@ const page = () => {
 
   return (
     <div>
-      <h1>Product List</h1>
+       {loading && ( 
+      <div className="loading-overlay">
+        <BarLoader  color={"#d60096"} loading={loading} size={350} className="loading-spinner" />
+      </div>
+        )}
+      <h1 className="ml-5 my-5">Product List</h1>
       {products.length === 0 ? (
-        <p>No products found</p>
+        <p className="ml-5">No products found</p>
       ) : (
-        <div className="products">
+        <div className="products px-2 md:p-0">
         {products.map(product => (
-          <div className="border w-48 block rounded-lg bg-white shadow-secondary-1 dark:bg-surface-dark" key={product.id}>
+          <div className="border my-2 mx-1 w-48 block rounded-lg bg-white shadow-secondary-1 dark:bg-surface-dark" key={product.id}>
             <img className="rounded-t-lg" src={product.imageUrl} alt={product.name} width="200" />
-            <div className="p-6 text-surface dark:text-black">
-            <h2 className="p-6 text-surface dark:text-black">{product.name}</h2>
-            <p className="p-6 text-surface dark:text-black">Price: ${product.price}</p>
-            <p className="p-6 text-surface dark:text-black">Description: {product.description}</p>
-            <p className="p-6 text-surface dark:text-black">Category: {product.category}</p>
+            <div className="text-surface dark:text-black">
+            <h2 className="text-surface dark:text-black">{product.name}</h2>
+            <p className="text-surface dark:text-black">Price: ${product.price}</p>
+            <p className="text-surface dark:text-black">Description: {product.description}</p>
+            <p className="text-surface dark:text-black">Category: {product.category}</p>
             </div>
           </div>
         ))}
