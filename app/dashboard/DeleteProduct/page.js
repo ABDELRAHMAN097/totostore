31683e8ad1f7962af8768f5578582855ab486bd0
boody <bodymagdy97@gmail.com>
2025-02-05 18:32:12 +0200

    es

--- app/dashboard/DeleteProduct/page.js
+++ app/dashboard/DeleteProduct/page.js
@@ -1,17 +1,13 @@
 "use client";
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { db } from "../../firebase";
-import {
-  collection,
-  getDocs,
-  deleteDoc,
-  doc,
-} from "firebase/firestore";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+import { BarLoader } from "react-spinners";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import ProtectedRoute from "../../ProtectedRoute/page";
 import { useRouter } from "next/navigation";
 import { useUser } from "../../context/UserContext";
-import SearchBar from '@/app/Components/SearchBar ';
+import SearchBar from "@/app/Components/SearchBar ";
 
 const DeleteProduct = () => {
   const [products, setProducts] = useState([]);
@@ -39,7 +35,9 @@ const DeleteProduct = () => {
     try {
       await deleteDoc(doc(db, "products", productId));
       setProducts(products.filter((product) => product.id !== productId));
-      setFilteredProducts(filteredProducts.filter((product) => product.id !== productId)); // تحديث المنتجات المفلترة بعد الحذف
+      setFilteredProducts(
+        filteredProducts.filter((product) => product.id !== productId)
+      ); // تحديث المنتجات المفلترة بعد الحذف
       console.log("تم حذف المنتج بالمعرف:", productId);
     } catch (error) {
       console.error("خطأ في حذف المنتج:", error);
@@ -56,38 +54,65 @@ const DeleteProduct = () => {
   }, [userRole, loading, router]);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="loading-overlay">
+        <BarLoader color={"#d60096"} loading={loading} size={350} />
+      </div>
+    );
   }
 
   return (
     <ProtectedRoute adminOnly={true}>
       <div>
-        <SearchBar products={products} setFilteredProducts={setFilteredProducts} />
-        
+        <SearchBar
+          products={products}
+          setFilteredProducts={setFilteredProducts}
+        />
+
         {/* Products Section */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-5">
+        <div className="flex flex-col gap-2 mx-4">
           {filteredProducts.length === 0 ? (
             <p>No Products Available</p>
           ) : (
             filteredProducts.map((product) => (
-              <div key={product.id} className="bg-white p-4 rounded-lg shadow-md">
-                <img
-                  src={product.imageUrl}
-                  alt={product.name}
-                  className="h-48 w-full object-cover rounded-lg mb-4"
-                />
-                <h2 className="text-xl font-bold">{product.name}</h2>
-                <p className="text-gray-500">Price: ${product.price}</p>
-                <p className="text-gray-500">
-                  Description: {product.description}
-                </p>
-                <p className="text-gray-500">Category: {product.category}</p>
-                <button
-                  className="mt-4 w-full text-center p-2 bg-red-500 text-white rounded-lg"
-                  onClick={() => handleDeleteProduct(product.id)}
-                >
-                  <RiDeleteBin5Fill className="w-full text-center" />
-                </button>
+              <div
+                key={product.id}
+                className="flex flex-row items-center justify-between w-full bg-slate-100 p-1 rounded-lg shadow-md"
+              >
+                <div className="flex-shrink-0">
+                  <img
+                    src={product.imageUrl}
+                    alt={product.name}
+                    className="w-[50px] h-[50px] object-cover rounded-lg"
+                  />
+                </div>
+
+                <div className="flex-1 ml-4">
+                  <h2 className="text-xl font-bold">{product.name}</h2>
+                </div>
+
+                <div className="flex-1 ml-4">
+                  <p className="text-gray-500">Price: ${product.price}</p>
+                </div>
+
+                <div className="flex-1 ml-4">
+                  <p className="text-gray-500">
+                    Description: {product.description}
+                  </p>
+                </div>
+
+                <div className="flex-1 ml-4">
+                  <p className="text-gray-500">Category: {product.category}</p>
+                </div>
+
+                <div>
+                  <button
+                    className="mt-4 text-center p-2 bg-red-500 text-white rounded-lg"
+                    onClick={() => handleDeleteProduct(product.id)}
+                  >
+                    <RiDeleteBin5Fill className="w-full text-center" />
+                  </button>
+                </div>
               </div>
             ))
           )}
@@ -95,6 +120,6 @@ const DeleteProduct = () => {
       </div>
     </ProtectedRoute>
   );
-}
+};
 
 export default DeleteProduct;
