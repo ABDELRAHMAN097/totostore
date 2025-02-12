a3e6b88fd34b4603221aab9604019bd0afab1bc2
Boody Magdy <106384468+ABDELRAHMAN097@users.noreply.github.com>
2025-02-12 09:47:55 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -106,7 +106,7 @@ const page = () => {
                     <div className="relative text-right p-2">
                       <h2
                         dir="rtl"
-                        className="text-gray-800 font-normal text-sm leading-none mb-2 overflow-hidden text-ellipsis line-clamp-2"
+                        className="text-gray-800 font-normal text-sm leading-none mb-1 py-1 overflow-hidden text-ellipsis line-clamp-2"
                       >
                         {product.description}
                       </h2>
